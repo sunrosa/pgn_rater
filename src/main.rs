b49433c49d3a0269945467318da7ad415328315d
Ibis Sunrosa <drsunrosa@gmail.com>
2024-05-19 07:43:34 +0000

    Actually sort games by date (lol)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use std::{borrow::Borrow, collections::HashMap};
+use std::collections::HashMap;
 
+use chrono::NaiveDate;
+use pgn::OutcomeResult;
 use pgn_reader::Color;
 use skillratings::glicko2::{self, Glicko2Config, Glicko2Rating};
 
 mod pgn;
 
 fn main() {
+    // Raw PGN buffer
     let mut pgn = pgn::from_file("all.pgn").unwrap();
 
+    // Glicko-2 data
     let mut g2_players: HashMap<String, Glicko2Rating> = HashMap::new();
     let g2_config = Glicko2Config::new();
 
+    // Actual data captured from PGN
+    let mut games: Vec<OutcomeResult> = Vec::new();
+
+    // Pull relevant information from PGN with visitor and error checking
     loop {
         let mut visitor = pgn::Outcome::default();
         let result = pgn.read_game(&mut visitor);
@@ -18,41 +26,64 @@ fn main() {
         match result {
             Ok(o) => match o {
                 Some(s) => match s {
-                    Ok(outcome) => {
-                        let wl = match outcome.2 {
-                            pgn_reader::Outcome::Decisive { winner } => match winner {
-                                Color::White => skillratings::Outcomes::WIN,
-                                Color::Black => skillratings::Outcomes::LOSS,
-                            },
-                            pgn_reader::Outcome::Draw => skillratings::Outcomes::DRAW,
-                        };
-
-                        let (w, b) = glicko2::glicko2(
-                            g2_players
-                                .get(&outcome.0)
-                                .unwrap_or(&Glicko2Rating::default()),
-                            g2_players
-                                .get(&outcome.1)
-                                .unwrap_or(&Glicko2Rating::default()),
-                            &wl,
-                            &g2_config,
-                        );
-
-                        g2_players.insert(outcome.0, w);
-                        g2_players.insert(outcome.1, b);
-                    }
-                    Err(e) => println!("PGN error: {}", e),
+                    Ok(outcome) => games.push(outcome),
+                    Err(e) => match e {
+                        pgn::OutcomeError::HeaderUtf8(e) => panic!("{}", e),
+                        pgn::OutcomeError::DateFormatting(e) => panic!("{}", e),
+                        _ => {}
+                    },
                 },
                 None => break,
             },
-            Err(e) => println!("IO error: {}", e),
+            Err(e) => eprintln!("IO error: {}", e),
         }
     }
 
-    let mut ratings_sorted = g2_players.into_iter().collect::<Vec<_>>();
+    // Sort games by date
+    games.sort_by(|a, b| a.date.cmp(&b.date));
+
+    // Rate all games
+    for game in games {
+        rate(game, &mut g2_players, g2_config);
+    }
+
+    // Sort players by rating descending and filter by RD
+    let mut ratings_sorted = g2_players
+        .into_iter()
+        .filter(|r| r.1.deviation < 200.)
+        .collect::<Vec<_>>();
     ratings_sorted.sort_by(|a, b| b.1.rating.total_cmp(&a.1.rating));
 
+    // Print all filtered player ratings
     for rating in ratings_sorted {
-        println!("{}: {}", rating.0, rating.1.rating);
+        println!("{}: {} ({})", rating.0, rating.1.rating, rating.1.deviation);
     }
+}
+
+fn rate(
+    outcome: OutcomeResult,
+    g2_players: &mut HashMap<String, Glicko2Rating>,
+    g2_config: Glicko2Config,
+) {
+    let wl = match outcome.outcome {
+        pgn_reader::Outcome::Decisive { winner } => match winner {
+            Color::White => skillratings::Outcomes::WIN,
+            Color::Black => skillratings::Outcomes::LOSS,
+        },
+        pgn_reader::Outcome::Draw => skillratings::Outcomes::DRAW,
+    };
+
+    let (w, b) = glicko2::glicko2(
+        g2_players
+            .get(&outcome.white)
+            .unwrap_or(&Glicko2Rating::default()),
+        g2_players
+            .get(&outcome.black)
+            .unwrap_or(&Glicko2Rating::default()),
+        &wl,
+        &g2_config,
+    );
+
+    g2_players.insert(outcome.white, w);
+    g2_players.insert(outcome.black, b);
 }
--- src/pgn.rs
+++ src/pgn.rs
@@ -1,5 +1,6 @@
 use std::{fs::File, str::Utf8Error};
 
+use chrono::{NaiveDate, ParseError};
 use pgn_reader::{BufferedReader, Visitor};
 
 pub fn from_file(path: &str) -> std::io::Result<BufferedReader<File>> {
@@ -8,17 +9,26 @@ pub fn from_file(path: &str) -> std::io::Result<BufferedReader<File>> {
     Ok(reader)
 }
 
+#[derive(Debug, Clone)]
+pub struct OutcomeResult {
+    pub white: String,
+    pub black: String,
+    pub date: NaiveDate,
+    pub outcome: pgn_reader::Outcome,
+}
+
 #[derive(Debug, Clone, Default)]
 pub struct Outcome {
     pub white: String,
     pub black: String,
+    pub date: Option<NaiveDate>,
     pub outcome: Option<pgn_reader::Outcome>,
 
     error: Option<OutcomeError>,
 }
 
 impl Visitor for Outcome {
-    type Result = Result<(String, String, pgn_reader::Outcome), OutcomeError>;
+    type Result = Result<OutcomeResult, OutcomeError>;
 
     fn header(&mut self, key: &[u8], value: pgn_reader::RawHeader<'_>) {
         if key == "White".as_bytes() {
@@ -39,6 +49,21 @@ impl Visitor for Outcome {
                 }
             };
         }
+        if key == "Date".as_bytes() {
+            self.date = match value.decode_utf8() {
+                Ok(o) => match NaiveDate::parse_from_str(&o, "%Y.%m.%d") {
+                    Ok(o) => Some(o),
+                    Err(e) => {
+                        self.error = Some(e.into());
+                        return;
+                    }
+                },
+                Err(e) => {
+                    self.error = Some(e.into());
+                    return;
+                }
+            }
+        }
     }
 
     fn outcome(&mut self, outcome: Option<pgn_reader::Outcome>) {
@@ -54,20 +79,24 @@ impl Visitor for Outcome {
     fn end_game(&mut self) -> Self::Result {
         match self.error {
             Some(s) => Err(s),
-            None => Ok((
-                self.white.clone(),
-                self.black.clone(),
-                self.outcome.expect("Unreachable."),
-            )),
+            None => Ok(OutcomeResult {
+                white: self.white.clone(),
+                black: self.black.clone(),
+                outcome: self.outcome.expect("Unreachable."),
+                date: self.date.unwrap(),
+            }),
         }
     }
 }
 
 #[derive(Debug, Clone, Copy, thiserror::Error)]
 pub enum OutcomeError {
-    #[error("Error decoding utf8 in header")]
+    #[error("Error decoding utf8 in header: {0}")]
     HeaderUtf8(#[from] Utf8Error),
 
     #[error("No outcome found in game")]
     NoOutcome,
+
+    #[error("Date formatting error: {0}")]
+    DateFormatting(#[from] ParseError),
 }
