807fd8a70084a645cec7639889d20b20282f77d9
Ibis Sunrosa <drsunrosa@gmail.com>
2024-05-19 05:51:55 +0000

    Rating

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use std::{borrow::Borrow, collections::HashMap};
+
+use pgn_reader::Color;
+use skillratings::glicko2::{self, Glicko2Config, Glicko2Rating};
+
 mod pgn;
 
 fn main() {
     let mut pgn = pgn::from_file("all.pgn").unwrap();
+
+    let mut g2_players: HashMap<String, Glicko2Rating> = HashMap::new();
+    let g2_config = Glicko2Config::new();
 
     loop {
         let mut visitor = pgn::Outcome::default();
@@ -10,8 +18,28 @@ fn main() {
         match result {
             Ok(o) => match o {
                 Some(s) => match s {
-                    Ok(o) => {
-                        println!("{:?}", o);
+                    Ok(outcome) => {
+                        let wl = match outcome.2 {
+                            pgn_reader::Outcome::Decisive { winner } => match winner {
+                                Color::White => skillratings::Outcomes::WIN,
+                                Color::Black => skillratings::Outcomes::LOSS,
+                            },
+                            pgn_reader::Outcome::Draw => skillratings::Outcomes::DRAW,
+                        };
+
+                        let (w, b) = glicko2::glicko2(
+                            g2_players
+                                .get(&outcome.0)
+                                .unwrap_or(&Glicko2Rating::default()),
+                            g2_players
+                                .get(&outcome.1)
+                                .unwrap_or(&Glicko2Rating::default()),
+                            &wl,
+                            &g2_config,
+                        );
+
+                        g2_players.insert(outcome.0, w);
+                        g2_players.insert(outcome.1, b);
                     }
                     Err(e) => println!("PGN error: {}", e),
                 },
@@ -20,4 +48,6 @@ fn main() {
             Err(e) => println!("IO error: {}", e),
         }
     }
+
+    println!("{:?}", g2_players);
 }
